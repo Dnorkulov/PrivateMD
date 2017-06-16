3ca2cf1b585f36b5f3a2becd599186723f727c8f
Daler <dalernorkulov@gmail.com>
2017-06-16 13:04:04 -0400

    1.2

--- app/src/main/java/com/dalernorkulov/privatemd/MainActivity.java
+++ app/src/main/java/com/dalernorkulov/privatemd/MainActivity.java
@@ -1,7 +1,14 @@
 package com.dalernorkulov.privatemd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.R.attr.button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Button create = (Button) findViewById(R.id.create);
+        create.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent createIntent = new Intent(MainActivity.this, CreateUser.class);
+                startActivity(createIntent);
+            }
+        });
+        EditText name = (EditText)findViewById(R.id.name);
     }
-
 }
+
